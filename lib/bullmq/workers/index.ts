67c70474f681d9c8bfb1eab3674d1a06bd45b711
Cohortech <jhordanmeza77@gmail.com>
2026-06-16 06:43:55 -0500

    Add uncaughtException handler and keepalive to worker entry

--- lib/bullmq/workers/index.ts
+++ lib/bullmq/workers/index.ts
@@ -2,3 +2,14 @@ import "@/lib/bullmq/workers/nlp-worker";
 import "@/lib/bullmq/workers/cohort-worker";
 
 console.log("Workers de Cohortech iniciados.");
+
+process.on("uncaughtException", (err) => {
+  console.error("uncaughtException:", err);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("unhandledRejection:", reason);
+});
+
+// Keep process alive
+setInterval(() => {}, 30_000);
